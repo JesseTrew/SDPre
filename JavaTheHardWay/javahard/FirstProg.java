5c47e20cdc08a964eda5faf2e385f4673fbb1caa
Jesse Trew <jesse.trew@gmail.com>
2019-08-25 11:35:27 -0500

    Add JTHW Exercises 1-4

--- JavaTheHardWay/javahard/FirstProg.java
+++ JavaTheHardWay/javahard/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
     public static void main( String [] args) {
-        System.out.println( "I am determined to learn how to code." );
-        System.out.println( "Today's date is" );
+        System.out.println( "I, Jesse Trew, am determined to learn how to code." );
+        System.out.println( "Today's date is Sunday, August 25, 2019." );
     }
 }
---
+++ JavaTheHardWay/javahard/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+    public static void main(String[] args) {
+        System.out.println("+------------------------+");
+        System.out.println("|                        |");
+        System.out.println("|      CORNER STORE      |");
+        System.out.println("|                        |");
+        System.out.println("| 2019-08-25  10:01AM    |");
+        System.out.println("|                        |");
+        System.out.println("| Gallons:       10.870  |");
+        System.out.println("| Price/gallon: $ 2.259  |");
+        System.out.println("|                        |");
+        System.out.println("| Fuel total:  $ 24.56   |");
+        System.out.println("|                        |");
+        System.out.println("+------------------------+");
+    }
+}
