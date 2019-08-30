29b51bfb356872afec9e6d9ffd52e48c03b4b14b
Jesse Trew <jesse.trew@gmail.com>
2019-08-29 19:29:42 -0500

    Add Objective 6 Labs

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4{
+    public static void main(String[] args){
+        int sum = 0, count = 1;
+
+        while (count <= 20){
+            sum = count + sum;
+            count = count+1;
+        }
+        System.out.println(sum);
+    }
+}
